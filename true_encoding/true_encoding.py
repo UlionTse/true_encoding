b2707a53cb54b02374a1642929ef94fa76bdd9bb
UlionTse <shinalone@outlook.com>
2017-11-22 01:21:13 +0800

    Update true_encoding.py

--- true_encoding/true_encoding.py
+++ true_encoding/true_encoding.py
@@ -1,14 +1,16 @@
 import re
 
 def Tcode(response):
-    # space
-    if ' charset="' in response.text:
+    if ' charset="' in response.text: # space
         pattern = re.compile('charset="(.*?)"',re.S)
         return re.findall(pattern,response.text)[0]
+    
+    elif ('charset=utf-8' or 'charset=UTF-8') in response.text:
+        return 'utf-8'
     elif ('charset=gb2312' or 'charset=GB2312') in response.text:
         return 'GB2312'
     elif ('charset=gbk' or 'charset=GBK') in response.text:
-        return 'gbk'
+        return 'GBK'
     elif ('charset=cp936' or 'charset=CP936') in response.text:
         return 'cp936'
     elif ('charset=ascii' or 'charset=ASCII') in response.text:
