4b888d29cd1e5fab9e85f0d80e3cf5ac56642130
UlionTse <shinalone@outlook.com>
2017-11-27 16:54:11 +0800

    Update true_encoding.py

--- true_encoding/true_encoding.py
+++ true_encoding/true_encoding.py
@@ -1,15 +1,15 @@
 import re
 
-def encode_bug(res):
-    if res.encoding == 'ISO-8859-1':
-        if 'charset="' in res.text:
+def encode_bug(self,res):
+    if ((res.encoding == 'ISO-8859-1') or (res.encoding is None) or ('charset=gb2312' in res.text) or ('charset=GB2312' in res.text)):
+        if (('charset=gb2312' in res.text) or ('charset=GB2312' in res.text)):#people.cn
+            return 'gb2312'
+        elif 'charset="' in res.text:
             pattern = re.compile('charset="(.*?)"', re.S)
             res.encoding = re.findall(pattern, res.text)[0]
             return res.encoding
         elif (('charset=utf-8' in res.text) or ('charset=UTF-8' in res.text)):
             return 'utf-8'
-        elif (('charset=gb2312' in res.text) or ('charset=GB2312' in res.text)):
-            return 'gb2312'
         elif (('charset=GBK' in res.text) or ('charset=gbk' in res.text)):
             return 'GBK'
         elif (('charset=cp936' in res.text) or ('charset=CP936' in res.text)):
